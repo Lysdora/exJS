f69256548c9aa760f9d8107f7be499427984750b
Lysdora Guicherd <lysdora.web@gmail.com>
2017-11-15 12:03:44 +0100

    add manege.js

---
+++ js/manege.js
@@ -0,0 +1,25 @@
+// Tournez manège
+/*
+Ecrivez un programme manege.js qui fait faire 10 tours de manège en affichant un message à chaque tour.
+ */
+
+ //var manege = Number(prompt("Allez ! on va faire des tours de maneges, combien de tours petit noob ?"));
+ //var i = 0;
+
+// code avec while
+
+/*
+while (i < manege) {
+    i++;
+    console.log("C'est le tour de manège numéro " + i );
+    console.log("^.^^.^^.^^.^^.^^.^^.^^.^^.^^.^^.^^.^^.^^.^^.^^.^ ");
+
+}*/
+
+// code avec for
+
+/*for (var i = 1; i <= manege ; i++) {
+    console.log("C'est le tour de manège numéro " + i );
+    console.log("^.^^.^^.^^.^^.^^.^^.^^.^^.^^.^^.^^.^^.^^.^^.^^.^ ");
+}*/
+
---
+++ js/test.js
@@ -0,0 +1,15 @@
+/*
+console.log("Debut du programme i");
+var i = 0;
+while (i <10) {
+    i++;
+    console.log(i);
+}
+console.log("Fin du programme i");
+
+console.log("Début du programme j");
+
+for (j = 20 ; j > 0; j--) {
+    console.log(j);
+}
+console.log("Début du programme j");*/
