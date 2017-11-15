b492c9a5137fdce13a9551e3e850298f4dd5ef1b
Lysdora Guicherd <lysdora.web@gmail.com>
2017-11-15 14:02:37 +0100

    add multiplication.js + code

---
+++ js/multiplication.js
@@ -0,0 +1,62 @@
+// Table de multiplication
+// Fait le 15/11/2017     [A REFAIRE]
+
+/*
+Ecrivez un programme multiplication.js qui fait saisir un nombre à l'utilisateur puis affiche la table de multiplication de ce nombre.
+ */
+// J'ai bidouillé, ca à l'air de fonctionner si j'appelle une function
+
+/*function tableMulti (num) {
+    console.log(num * 1);
+    console.log(num * 2);
+    console.log(num * 3);
+    console.log(num * 4);
+    console.log(num * 5);
+    console.log(num * 6);
+    console.log(num * 7);
+    console.log(num * 8);
+    console.log(num * 9);
+    console.log(num * 10);
+}
+
+var i = 2;
+// alors la c'est de la merdouille lol, j
+ //var helpers = "chocolatee";
+var multiplication = Number(prompt("Saisir un nombre entre 2 et 9"));
+ //var resultaaa = Number * helpers.length;
+
+
+while (i <=2  && i <= 9) {
+    i++;
+    if (multiplication !== 2) {
+    } else {
+        console.log("Voici la table de multiplication de 2:");
+        tableMulti(multiplication);
+
+    }
+    if (multiplication !== 3) {
+    } else {
+        console.log("Voici la table de multiplication de 3:");
+        tableMulti(multiplication);
+    }
+}*/
+
+
+
+// J'ai regardé la correction pour me corriger car j'ai pas reussis à 100%
+
+/*var nombre = 0; // Valeur par défaut pour entrer dans la boucle
+
+while (nombre < 2 || nombre > 9) {
+    nombre = Number(prompt("Entrez un nombre entre 2 et 9"));
+}
+console.log("Voici la table de multiplication de " + nombre);
+var i = 1;
+while (i <= 10) {
+    console.log(nombre + " x " + i + " = " + (nombre * i));
+    i++;
+}*/
+
+
+
+
