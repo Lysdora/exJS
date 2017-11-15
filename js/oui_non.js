3edd5c3d9c8ea3b6e64f48469c7db3be29b748f9
Lysdora Guicherd <lysdora.web@gmail.com>
2017-11-15 14:24:55 +0100

    add oui_non.js + code

---
+++ js/oui_non.js
@@ -0,0 +1,13 @@
+// Ni Oui ni Non
+// A REFAIRE
+/*var questionOuiNon = prompt("Voulez vous jouer au jeu du ni oui ni non ?");
+var i = questionOuiNon.length;
+
+while (questionOuiNon !== "oui" && questionOuiNon != "non" ) {
+    questionOuiNon = prompt("Voulez vous jouer au jeu du ni oui ni non ?");
+}
+console.log("Vous avez perdu !");*/
+
+
+
+
