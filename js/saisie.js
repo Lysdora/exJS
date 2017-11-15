1799627b60d0e9035eee2a318b01132ffb14f767
Lysdora Guicherd <lysdora.web@gmail.com>
2017-11-15 13:23:30 +0100

    add saisie.js

---
+++ js/saisie.js
@@ -0,0 +1,14 @@
+// Controle de saisie
+/*
+Ecrivez un programme saisie.js qui fait saisir un nombre à l'utilisateur jusqu'à ce que ce nombre soit inférieur ou égal à 100.
+
+Ensuite, améliorez votre programme pour que le nombre saisi soit compris entre 50 et 100.
+ */
+/*
+var num = 0;
+while (num < 50 || num > 100) {
+    num = Number(prompt("Saisir un nombre entre 50 et 100"));
+
+}
+
+console.log("Bravo votre nombre est le  " + num);*/
