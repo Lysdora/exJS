fec4928972a1355eb22fef4c475316dc2fafdf0a
Lysdora Guicherd <lysdora.web@gmail.com>
2017-11-15 07:28:28 +0100

    add ex jour suivant

--- jour_suivant.js
+++ jour_suivant.js
@@ -2,25 +2,26 @@
 
 var jour = prompt("Ecris un jour de la semaine sans majuscule");
 
-/*if (jour === "lundi") {
-    console.log("Demain, nous serons mardi");
-}else if (jour === "mardi") {
-    console.log("Demain, nous serons mercredi");
-}else if (jour === "mercredi") {
-    console.log("Demain, nous serons jeudi");
-}else if (jour === "jeudi") {
-    console.log("Demain, nous serons vendredi");
-}else if (jour === "vendredi") {
-    console.log("Demain, nous serons samedi");
-}else if (jour === "samedi") {
-    console.log("Demain, nous serons dimanche");
-}else if (jour === "dimanche") {
-    console.log("Demain, nous serons lundi");
+if (jour.toUpperCase() === "LUNDI") {
+    console.log("Demain, nous serons mardi.");
+}else if (jour.toUpperCase() === "MARDI") {
+    console.log("Demain, nous serons mercredi.");
+}else if (jour.toUpperCase() === "MERCREDI") {
+    console.log("Demain, nous serons jeudi.");
+}else if (jour.toUpperCase() === "JEUDI") {
+    console.log("Demain, nous serons vendredi.");
+}else if (jour.toUpperCase() === "VENDREDI") {
+    console.log("Demain, nous serons samedi.");
+}else if (jour.toUpperCase() === "SAMEDI") {
+    console.log("Demain, nous serons dimanche.");
+}else if (jour.toUpperCase() === "DIMANCHE") {
+    console.log("Demain, nous serons lundi.");
 }else {
-    console.log("Je n'ai pas compris, rafraichis ta page et ecris correctement le jour de la semaine en majuscule");
-}*/
+    console.log("Je n'ai pas compris, recommence.");
+}
 
 
+/*
 switch (jour) {
     case "lundi":
         console.log("Demain, nous serons mardi");
@@ -44,6 +45,6 @@ switch (jour) {
         console.log("Demain, nous serons lundi");
         break;
     default :
-        console.log("Je n'ai pas compris, rafraichis ta page et ecris correctement le jour de la semaine en majuscule");
+        console.log("Je n'ai pas compris, rafraichis ta page et ecris correctement le jour de la semaine sans majuscule");
         break;
-}
+}*/
