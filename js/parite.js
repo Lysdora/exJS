6c62d5b5d6bdd0a1629e9cf8f548bb484294e941
Lysdora Guicherd <lysdora.web@gmail.com>
2017-11-15 13:05:06 +0100

    add parite.js

---
+++ js/parite.js
@@ -0,0 +1,29 @@
+// Nombre pairs
+/*
+var parite = Number(prompt("Saisir un chiffre, la console va vous annoncé si le chiffre est pair ou impair"));
+
+
+for (var i = 1; i <= parite; i++) {
+    if (i % 2 === 0 || i % 2 === 1 ) {
+        console.log(parite + " est pair");
+        // j'ajoute le else if pour les nombres impair
+    } else {
+        console.log("J'ai pas compris");
+    }
+
+}*/
+
+/*for (var i = 1; i <= i; i++) {
+    if (i % 2 === 0 || i % 2 === 1 ) {
+        console.log(i + " est pair");
+        // j'ajoute le else if pour les nombres impair
+    } else {
+        console.log("J'ai pas compris");
+    }
+
+}*/
+
+
+
+
+
