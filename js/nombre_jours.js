bbbdbbf5c131c6ff351c190e2d7eb3fd0a767207
Lysdora Guicherd <lysdora.web@gmail.com>
2017-11-15 10:38:49 +0100

    add nombre_jours.js

---
+++ js/nombre_jours.js
@@ -0,0 +1,49 @@
+// Nombre de jours du mois
+/*
+Ecrivez un programme nombre_jours.js qui fait saisir le numéro d'un mois (nombre entre 1 et 12) puis affiche le nombre de jours de ce mois. On ne tiendra pas compte des années bissextiles. Les erreurs de saisie doivent être gérées.
+
+ */
+
+var nombreDeJourMois = Number(prompt("Entrez le numéros d'un mois"));
+
+switch (nombreDeJourMois) {
+    case 1 :
+        console.log("Il y a 31 jours au mois de janvier.");
+        break;
+    case 2 :
+        console.log("Il y a 28 jours au mois de février.");
+        break;
+    case 3 :
+        console.log("Il y a 31 jours au mois de mars.");
+        break;
+    case 4 :
+        console.log("Il y a 30 jours au mois d'avril.");
+        break;
+    case 5 :
+        console.log("Il y a 31 jours au mois de mai.");
+        break;
+    case 6 :
+        console.log("Il y a 30 jours au mois de juin.");
+        break;
+    case 7 :
+        console.log("Il y a 31 jours au mois de juillet.");
+        break;
+    case 8 :
+        console.log("Il y a 31 jours au mois d'aout.");
+        break;
+    case 9 :
+        console.log("Il y a 30 jours au mois de septembre.");
+        break;
+    case 10 :
+        console.log("Il y a 31 jours au mois d'octobre.");
+        break;
+    case 11 :
+        console.log("Il y a 30 jours au mois de novembre.");
+        break;
+    case 12 :
+        console.log("Il y a 31 jours au mois de décembre.");
+        break;
+    default :
+        console.log("Je n'ai pas compris");
+        break;
+}
